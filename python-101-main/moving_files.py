4f72f5513c5a118a76e811ef6274d872ca163227
LemmyKM <karel.swinnen@protonmail.com>
2024-02-20 13:25:34 +0200

    correction

--- python-101-main/moving_files.py
+++ python-101-main/moving_files.py
@@ -16,4 +16,4 @@ scrnshot.mkdir(exist_ok=True)
 docs = pathlib.Path('/Users/lemmy/Documents')
 for file in docs.iterdir():
     if file.suffix == '.png':
-        shutil.move(docs, scrnshot)
+        shutil.move(file, scrnshot)

7b18805dc1d651a9611aea73386790becfd5218b
LemmyKM <karel.swinnen@protonmail.com>
2024-02-26 15:27:06 +0200

    update

--- python-201-main/02_more-datatypes/02_11_set_basics.py
+++ python-201-main/02_more-datatypes/02_11_set_basics.py
@@ -4,12 +4,13 @@
 
 s = {1, 2, 3, 4}
 t = {3, 4, 5, 6}
+u = [7, 8, 9, 10]
 
 intersect = s.intersection(t)
 print(intersect)
 #or
-intersect2 = s & t
-print(intersect2)
+# intersect2 = s & t
+# print(intersect2)
 
 
 together = s | t
@@ -17,3 +18,7 @@ print(together)
 #or
 together2 = s.union(t) # !!! met '.union()' kan je niet enkel 'sets' samenvoegen. bv ook 'lists' en 'sets'
 print(together2)
+
+u_to_set = set(u)
+t_en_u = t | u_to_set
+print(t_en_u)

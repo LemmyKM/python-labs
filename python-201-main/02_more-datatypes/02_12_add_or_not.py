f60c96ef08f4447a3c26343b668d631e16b62204
LemmyKM <karel.swinnen@protonmail.com>
2024-02-26 21:34:40 +0200

    update

--- python-201-main/02_more-datatypes/02_12_add_or_not.py
+++ python-201-main/02_more-datatypes/02_12_add_or_not.py
@@ -7,11 +7,28 @@
 # If the user loses 5 points, quit the program.
 # They win if they manage to create a set that has more than 10 items.
 
-input_list = set()
-allowed_input = range(1, 50)
-number = int(input('enter a number from 1 to 50 : '))
+import sys
 
-while number not in allowed_input:
-    number = input('that is not a number from 1 to 50. Try again : ')
-    
+no_dup = set()
+points = 5
 
+while True:
+    user_input = int(input('enter a figure between 1 and 50 : '))
+    while user_input > 50:
+        user_input = int(input('Not between 1 and 50. Try again : '))
+    if user_input in no_dup:
+        print('You already used this number. You loose 1 point.')
+        points -= 1
+        if points == 0:
+            print('All 5 points used. You loose!')
+            sys.exit()
+    elif user_input not in no_dup:
+        no_dup.update([user_input]) #update adds multiple items, 'add' adds only one.  # de haakjes '([]) om de 'user_input' om te zetten naar immutable eigenschap. Anders gaat die niet in de set()
+        if len(no_dup) == 11:
+            print('You win!')
+            sys.exit()  
+    print()
+    print(f"Points left : {points}")
+    print(f"Content of 'set' : {no_dup}")
+    print(f"Items in 'set' : {len(no_dup)}")
+    print()

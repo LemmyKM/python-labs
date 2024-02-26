e1d7a28030df9112f58a256a42fc2838b9c138c5
LemmyKM <karel.swinnen@protonmail.com>
2024-02-26 22:43:24 +0200

    update

--- python-201-main/02_more-datatypes/02_13_power_dict.py
+++ python-201-main/02_more-datatypes/02_13_power_dict.py
@@ -2,3 +2,9 @@
 # and values `n * n` for numbers 1 to 10. For example:
 # result = {1: 1, 2: 4, 3: 9, ... and so on}
 
+many_keys = {}
+
+for n in range(1, 10):
+    many_keys[n] = n * n
+
+print(many_keys)

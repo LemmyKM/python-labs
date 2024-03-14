05ce8f020e848cd822b575c871382975ab45cea9
LemmyKM <karel.swinnen@protonmail.com>
2024-03-14 10:03:45 +0200

    update

--- python-201-main/03_file-input-output/03_01_long_words.py
+++ python-201-main/03_file-input-output/03_01_long_words.py
@@ -1,2 +1,12 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
+
+
+from pathlib import Path
+
+file_path = Path.home() / 'Python/CODINGNOMADS/labs/python-201-main/03_file-input-output/words.txt'
+with open(file_path, mode='r', encoding='utf-8') as file:
+    word = file.readlines()
+    for w in word:
+        if len(w) > 20:
+            print(w, end='')

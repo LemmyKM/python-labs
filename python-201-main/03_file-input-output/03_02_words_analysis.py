c191f33af37ebed3d60f80b52d150a33e02649ea
LemmyKM <karel.swinnen@protonmail.com>
2024-03-14 10:06:37 +0200

    update

--- python-201-main/03_file-input-output/03_02_words_analysis.py
+++ python-201-main/03_file-input-output/03_02_words_analysis.py
@@ -4,3 +4,28 @@
 # 1. The shortest word (if there is a tie, print all)
 # 2. The longest word (if there is a tie, print all)
 # 3. The total number of words in the file.
+
+
+from pathlib import Path
+
+lengte = []
+
+file_path = Path.home() / 'Python/CODINGNOMADS/labs/python-201-main/03_file-input-output/words.txt'
+with open(file_path, mode='r', encoding='utf-8') as file:
+    words = file.readlines()
+    total_words = len(words)
+    for w in words:
+        lengte_word = lengte.append(len(w))
+        if len(w) == 3:
+            print(w, end='')
+        elif len(w) == 22:
+            print(w, end='')
+        else:
+            pass
+
+kortst = min(lengte)
+langst = max(lengte)
+
+print(f"Total number of words = {total_words}")
+print(f"Shortest word = {kortst}")
+print(f"Longest word = {langst}")

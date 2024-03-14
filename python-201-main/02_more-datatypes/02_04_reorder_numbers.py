62b19db9643cdc6ba07e8d7c3c13b31535a43975
LemmyKM <karel.swinnen@protonmail.com>
2024-03-14 09:29:29 +0200

    update

--- python-201-main/02_more-datatypes/02_04_reorder_numbers.py
+++ python-201-main/02_more-datatypes/02_04_reorder_numbers.py
@@ -7,10 +7,20 @@
 # Example input:  1,2,3,4,5,6,7,8,9,10
 # Example output: 2,4,6,8,10,9,7,5,3,1
 
-lijst = ['woord', 5.0, 'treshold', 987, 'vort', 'dikwijls', 12.4, 'bumper']
+lijst = [1,2,3,4,5,6,7,8,9,10]
+even_lijst = []
+oneven_lijst = []
 for x, y in enumerate(lijst, start=1):
     if x % 2 == 0:
-        print(y)
-        
-lijst.reverse()
-print(lijst)
+        even_lijst.append(y)
+print(even_lijst)
+
+for x, y in enumerate(lijst, start=1):
+    if x % 2 != 0:
+        oneven_lijst.append(y)
+
+omgekeerd = oneven_lijst[::-1]
+print(even_lijst + omgekeerd)
+
+
+

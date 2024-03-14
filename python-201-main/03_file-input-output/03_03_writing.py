58d4196bc536c4e23335e3b54f81362c04f07b4f
LemmyKM <karel.swinnen@protonmail.com>
2024-03-14 10:08:01 +0200

    update

--- python-201-main/03_file-input-output/03_03_writing.py
+++ python-201-main/03_file-input-output/03_03_writing.py
@@ -1,2 +1,14 @@
 # Write a script that reads in the contents of `words.txt`
 # and writes the contents in reverse to a new file `words_reverse.txt`.
+
+
+from pathlib import Path
+
+file_path = Path.home() / 'Python/CODINGNOMADS/labs/python-201-main/03_file-input-output/words.txt'
+new_file = Path.home() / 'Python/CODINGNOMADS/labs/python-201-main/03_file-input-output/words_reverse.txt'
+
+with open(file_path, mode='r', encoding='utf-8') as file:
+    tekst = file.readlines()
+
+with open(new_file, mode='w', encoding='utf-8') as w_reverse:
+    w_reverse.writelines(tekst[::-1])

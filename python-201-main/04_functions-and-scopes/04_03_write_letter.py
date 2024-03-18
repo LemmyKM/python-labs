2a1db4e467b442dbf33c286e38ca3dcbd2349eca
LemmyKM <karel.swinnen@protonmail.com>
2024-03-18 12:53:53 +0200

    update

--- python-201-main/04_functions-and-scopes/04_03_write_letter.py
+++ python-201-main/04_functions-and-scopes/04_03_write_letter.py
@@ -3,3 +3,12 @@
 # message with the `name` input, as well as a goodbye message that uses
 # the `name` again. Combine that with the input `text` to return a
 # complete `letter`.
+
+
+def write_letter(name, text):
+    greeting = f"Hello {name}, '\n' {text} Stay well and we hope to see you there {name}."
+    return greeting 
+
+print(write_letter(
+            'Karel', 'We are pleased to announce that you have been nominated for the Pulitzer price."\n"'))
+

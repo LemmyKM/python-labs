fda14b7c3aff43162cf8630d387de5738ce27bc0
LemmyKM <karel.swinnen@protonmail.com>
2024-04-26 09:44:34 +0200

    update

--- python-201-main/08_debugging/08_02_find_the_bugs.py
+++ python-201-main/08_debugging/08_02_find_the_bugs.py
@@ -1,6 +1,25 @@
 # The code below contains a few bugs. Use your debugger to find and fix them.
 
-unsorted_list = [('first_element', 4), ('second_element', 2), ('third_element', 6)]
+# unsorted_list = [('first_element', 4), ('second_element', 2), ('third_element', 6)]
+# sorted_list = []
+
+# for x in range(1, len(unsorted_list)):
+
+#     minimum = unsorted_list[0][0]
+#     index = 0
+
+#     for i in range(1, len(unsorted_list)):
+#         if unsorted_list[i][1] < minimum:
+#             minimum = unsorted_list[i][i]
+#             index = i
+#     sorted_list.append(unsorted_list[index])
+#     unsorted_list.remove(unsorted_list[index])
+
+# print(sorted_list)
+
+
+# correction: incompatibility between int/str in 'unsorted_list'.
+unsorted_list = [(1, 4), (2, 2), (3, 6)]
 sorted_list = []
 
 for x in range(1, len(unsorted_list)):

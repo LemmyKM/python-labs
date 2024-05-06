760745eae902adf83d1764698439a56aa47036cc
LemmyKM <karel.swinnen@protonmail.com>
2024-05-06 16:43:32 +0200

    update

--- python-101-main/07_variables-and-types/07_06_whats_my_type.py
+++ python-101-main/07_variables-and-types/07_06_whats_my_type.py
@@ -1,7 +1,7 @@
 # What data type is the variable `mystery` at the end of the script?
 # What data types does it hold at certain points earlier in the script?
 
-mystery = None
-mystery = "Sommerfeld"
-mystery = 137
-mystery = mystery + 0.0
+mystery = None  #NoneType
+mystery = "Sommerfeld" # string
+mystery = 137 # integer
+mystery = mystery + 0.0 # float

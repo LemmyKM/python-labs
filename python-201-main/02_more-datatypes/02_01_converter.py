45059948cd08226dfc1faca993a4e98edf422562
LemmyKM <karel.swinnen@protonmail.com>
2024-05-07 14:55:01 +0200

    update

--- python-201-main/02_more-datatypes/02_01_converter.py
+++ python-201-main/02_more-datatypes/02_01_converter.py
@@ -1,11 +1,11 @@
 # Convert a string to a tuple and print out the result.
-# What do you see?
-# What happens if you try to iterate over your tuple of characters?
+# What do you see?  The printout stays on one line with every letter between brackets.
+# What happens if you try to iterate over your tuple of characters?  Every letter is printed out on a new line.
 # Do you notice any difference to iterating over the string?
 
 string = "codingnomads"
 string_tup = tuple(string)
 print(string_tup)
 
-for char in string:
+for char in string_tup:
     print(char)
---
+++ python-201-main/06_advanced-python-concepts/koken/recipes/soup.py
@@ -0,0 +1,8 @@
+# soup.py
+def make_soup(ingredient):
+    soup = f"{ingredient} soup"
+    print(f"You've made {soup}")
+    return soup
+
+from ingredients import carrot
+print(carrot)

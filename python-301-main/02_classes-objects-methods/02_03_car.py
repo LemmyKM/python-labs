bcf9aa9dbe1efbeb083535394c0acdcc3245e1a9
LemmyKM <karel.swinnen@protonmail.com>
2024-05-22 05:57:35 +0200

    update

--- python-301-main/02_classes-objects-methods/02_03_car.py
+++ python-301-main/02_classes-objects-methods/02_03_car.py
@@ -6,3 +6,30 @@
 #
 # Create at least two different objects of this `Car()` class and demonstrate
 # changing the objects' attributes.
+
+
+class Car:
+    def __init__(self, model, year, max_speed):
+        self.model = model
+        self.year = year
+        self.max_speed = max_speed
+
+    def increase_speed(self):
+        inc_speed = self.max_speed + 5
+        return inc_speed
+    
+    def doors(self):
+        return "number of doors = 4"
+    
+    def color(self):
+        return 'Color is yellow with blue dots.'
+    
+    def __str__(self):
+        return f"Car model : {self.model}\nYear of construction : {self.year}\nMax speed : {self.max_speed}\n"
+print()
+car = Car('Toyota Prius', 2015, 160)
+print(car)
+
+print(car.increase_speed())
+print(car.doors())
+print(car.color())
--- python-301-main/02_classes-objects-methods/02_04_classy_shapes.py
+++ python-301-main/02_classes-objects-methods/02_04_classy_shapes.py
@@ -5,3 +5,36 @@
 # Write methods in the appropriate class so that you can calculate
 # the area of both the rectangle and the circle, the perimeter
 # of the rectangle, and the circumference of the circle.
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return (self.length * 2) + (self.width * 2)
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius**2
+
+    def circumference(self):
+        return 2 * 3.14 * self.radius
+
+
+
+rectangle = Rectangle(3, 4)
+print(f"The area for the rectangle is {rectangle.area()}cm2.")
+print(f"The perimeter for the rectangle is {rectangle.perimeter()}cm.")
+print()
+
+circle = Circle(4)
+print(f"The area for the circle is {circle.area()}cm2.")
+print(f"The circumference for the circle is {circle.circumference()}cm.")

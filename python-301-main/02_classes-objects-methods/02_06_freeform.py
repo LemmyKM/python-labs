e054ece3d65f21f6bda189c72022e4ccf64e2e41
LemmyKM <karel.swinnen@protonmail.com>
2024-05-25 17:32:20 +0200

    update

--- python-301-main/02_classes-objects-methods/02_06_freeform.py
+++ python-301-main/02_classes-objects-methods/02_06_freeform.py
@@ -11,13 +11,51 @@
 # Using objects you can model anything you want:
 # Animals, paintings, card games, sports teams, trees, people etc...
 
+from time import sleep
+
 class Dishwasher:
-    def __init__(self, turn)
+    def __init__(self, cutlery, glasses, mugs, plates):
+        self.cutlery = cutlery
+        self.glasses = glasses
+        self.mugs = mugs
+        self.plates = plates
+
+    def __str__(self):
+       return f"The content of the dishwasher is {self.cutlery} pieces of cutlery, {self.glasses} glasses, {self.mugs} mugs and {self.plates} plates."
+
+    def start(self):
+        return 'the dishwasher is on and has started ...'
+        # time.sleep(6)
+        # print('Dishes are clean!')
+
+dishes = Dishwasher(17, 12, 11, 16)
+print(dishes)
+cycle = dishes.start()
+print(cycle)
 
 
 class Bike:
-    pass
+    def __init__(self, brakes, tyres, speed):
+        self.brakes = brakes
+        self.tyres = tyres
+        self.speed = speed
+
+    def __str__(self):
+        return f"Your bike's brake power is at {self.brakes}%, the tyres are {self.tyres} and you're going at {self.speed} km/h."
+
+        
+
+wheels = Bike('faulty', 'flat', 60)
+print(wheels)
 
 
-class Parrot:
-    pass
+
+class Laptop:
+    def __init__(self, power, temperature, screen):
+        self.power = power
+        self.temperature = temperature
+        self.screen = screen
+
+    def __str__(self):
+        pass
+    

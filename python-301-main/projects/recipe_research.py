1c406aac269395e313965c1d5560b7a999f299b0
LemmyKM <karel.swinnen@protonmail.com>
2024-05-27 20:28:40 +0200

    update

--- python-301-main/projects/recipe_research.py
+++ python-301-main/projects/recipe_research.py
@@ -1,3 +1,19 @@
 # Use your `Ingredients` class to create a URL to an online search
 # that allows to look for recipes for dishes made from the
 # available ingredients.
+
+import webbrowser
+# import requests
+
+class Ingredient:
+    def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount
+
+    def get_info(self):
+        self.wiki = f"https://en.wikipedia.org/wiki/{self.name}"
+#        response = requests.get(self.wiki)  no need for the requests module with 'webbrowser'
+        webbrowser.open(self.wiki, new=2)
+
+recipe = Ingredient('Rice', 11)
+print(recipe.get_info())

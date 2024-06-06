9f1ae92709e8074a6805883377c0af602fb8bda7
LemmyKM <karel.swinnen@protonmail.com>
2024-06-06 19:14:36 +0200

    update

--- python-301-main/projects/RPG_game.py
+++ python-301-main/projects/RPG_game.py
@@ -18,6 +18,7 @@ class Hero:
         print(f"{opponent.name} raises powerfield ...")
         time.sleep(3)
         print(f"{opp_dice}...")
+        time.sleep(2)
 
         if hero_dice >= opp_dice:
             print("Our knight prevails and lives to see another day!")
@@ -41,12 +42,14 @@ def start_game():
 
     while True:
         current_opponent = random.choice(opponents)
-        print(f"The gates of Hell are opened and {current_opponent.name} with a power level of {current_opponent.level} materialises in front of {hero.name}.\n")
+        print()
+        print(f"The gates of Hell are opened and {current_opponent.name} with a power level of {current_opponent.level} materialises in front of {hero.name}.")
         act = input("Do you want to [s]trike, (f)lee or (l)ook around? ")
+        print()
         while act not in ['s', 'f', 'l', 'q']:
             print('please enter [s, f, l]')
             print('To quit the game enter [q]')
-            act = input("Do you want to [s]trike, (f)lee or (l)ook around?")
+            act = input("Do you want to [s]trike, (f)lee or (l)ook around? ")
 
         if act == 's':
             if hero.attack(current_opponent):
@@ -58,7 +61,7 @@ def start_game():
             print(f"These sandals are not suited to take on {current_opponent.name}. I will fight him another day when I am wearing my allstars.")
         
         elif act == 'l':
-            print(f"{hero.name} sees the light, and also sees {current_opponent} with power level {current_opponent.level}")
+            print(f"{hero.name} sees the light, and also sees {current_opponent.name} with power level {current_opponent.level}")
 
         elif act == 'q':
             print('See you again on Easter day...')

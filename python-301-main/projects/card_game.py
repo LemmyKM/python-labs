5f2d6f25e6f4287aa66d10aa46c31d6a7edd52c6
LemmyKM <karel.swinnen@protonmail.com>
2024-06-18 18:04:50 +0200

    update

--- python-301-main/projects/card_game.py
+++ python-301-main/projects/card_game.py
@@ -1,6 +1,15 @@
 class Card:
-    suits = ["Clubs"]
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    ranks = ["None", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
     def __init__(self, suit=0, rank=0):
         self.suit = suit
         self.rank = rank
+
+    def __str__(self):
+        return (self.ranks[self.rank] + " of " + self.suits[self.suit])
+
+
+
+kaart = Card(2, 4)
+print(kaart)

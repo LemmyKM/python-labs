dc24fdba9687eb8c3b0ee311123b317e367415ed
LemmyKM <karel.swinnen@protonmail.com>
2024-06-20 12:07:10 +0200

    update

--- python-301-main/projects/card_game.py
+++ python-301-main/projects/card_game.py
@@ -9,7 +9,24 @@ class Card:
     def __str__(self):
         return (self.ranks[self.rank] + " of " + self.suits[self.suit])
 
+    def cmp(self, other):  # verander cmp later naar "value"
+        # check the suits
+        if self.suit > other.suit:
+            return 1
+        if self.suit < other.suit:
+            return -1
+        # suits are the same ... check ranks
+        if self.rank > other.rank:
+            return 1
+        if self.rank < other.rank:
+            return -1
+        # ranks are the same ...it's a tie
+        return 0
+    
 
+
+
+    
 
 kaart = Card(2, 4)
 print(kaart)

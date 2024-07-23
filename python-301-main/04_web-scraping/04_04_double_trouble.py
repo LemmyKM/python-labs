89e04ff780522ed2b5ce03e43ae955d57bfab9cb
LemmyKM <karel.swinnen@protonmail.com>
2024-07-23 08:30:18 +0200

    upload

--- python-301-main/04_web-scraping/04_04_double_trouble.py
+++ python-301-main/04_web-scraping/04_04_double_trouble.py
@@ -4,3 +4,32 @@
 # data that you receive from both of them.
 # E.g.: You could use the Ghibli API to find all ghosts from their films, and
 #       create an opposing team of Ghost Pokémon from the Poke API.
+
+
+
+import requests
+from pprint import pprint
+
+
+url = "https://weatherapi-com.p.rapidapi.com/astronomy.json"
+
+querystring = {"q":"Johannesburg"}
+
+headers = {
+	"x-rapidapi-key": "313ac4ab66msh88cd1f317e9a7eap1c128fjsn3bb8131661ce",
+	"x-rapidapi-host": "weatherapi-com.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+data = response.json()
+#pprint(data)
+
+def moon_sun():
+    print()
+    print(data['location']['name'])
+    print(data['location']['localtime'])
+    for k, v in data['astronomy']['astro'].items():
+        print(k,v)
+
+moon_sun()
